4e01a5db2f2d4ee4d66ae6670ff9c959d182dc5b
ynskmt <yrtiktwyr.0427@gmail.com>
2020-04-09 23:53:30 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 'use strict'
 
+// {
+//   const input = document.querySelector("input");
+//   const ul = document.querySelector("ul");
+//   const btn = document.querySelector("button");
+
+//   btn.addEventListener("click", function(){
+//     const li = document.createElement("li");
+//     li.textContent = input.value;
+//     ul.appendChild(li);
+//     input.value = "";
+//     input.focus();
+//   })
+
+// } 
+
 {
-  const input = document.querySelector("input");
-  const ul = document.querySelector("ul");
-  const btn = document.querySelector("button");
-
-  btn.addEventListener("click", function(){
+  document.querySelector("button").addEventListener("click", function(){
     const li = document.createElement("li");
-    li.textContent = input.value;
-    ul.appendChild(li);
-    input.value = "";
-    input.focus();
+    const select = document.querySelector("select");
+    li.textContent = select.value;
+    document.querySelector("ul").appendChild(li);
   })
-
 }
+
