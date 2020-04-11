23868c22734b9e82e6d52bd1b35dd6913aec7b70
ynskmt <yrtiktwyr.0427@gmail.com>
2020-04-11 19:47:19 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+'use strict'
 
+{
+  const input = document.querySelector("input");
+  const btn = document.querySelector("button");
+  const ul = document.querySelector("ul");
+
+  btn.addEventListener("click", function(){
+    const li = document.createElement("li");
+    li.textContent = input.value;
+    ul.appendChild(li);
+
+    input.value = "";
+    input.focus();
+  })
+}
